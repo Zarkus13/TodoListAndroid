b1802441b7da4405d9ad51fe8599026361703009
Alexis Weil <w.alexisweil@gmail.com>
2022-06-15 15:00:54 +0200

    Adds the possibility to select a task

--- app/src/main/java/com/cci/todolist/TodoListFragment.kt
+++ app/src/main/java/com/cci/todolist/TodoListFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.cci.todolist.databinding.FragmentTodoListBinding
 
 class TodoListFragment : Fragment() {
     private var _binding: FragmentTodoListBinding? = null
     private lateinit var binding: FragmentTodoListBinding
+
+    private var tasksCheckedNb = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +33,14 @@ class TodoListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.todoItemCheck1.setOnCheckedChangeListener { _, checked ->
+            selectTask(checked)
+        }
+
+        binding.todoItemCheck2.setOnCheckedChangeListener { _, checked ->
+            selectTask(checked)
+        }
+
         binding.todoItem1.setOnClickListener {
             val action = TodoListFragmentDirections
                 .actionTodoListFragmentToTodoItemFragment(
@@ -35,6 +49,21 @@ class TodoListFragment : Fragment() {
 
             findNavController().navigate(action)
         }
+    }
+
+    fun selectTask(checked: Boolean) {
+        if (checked)
+            this.tasksCheckedNb++
+        else
+            this.tasksCheckedNb--
+
+        binding.deleteButton.text = "Supprimer " + this.tasksCheckedNb + " tâches"
+
+        binding.deleteButton.visibility =
+            if (this.tasksCheckedNb > 0)
+                View.VISIBLE
+            else
+                View.INVISIBLE
     }
 
     override fun onDestroyView() {
